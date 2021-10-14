58fddc639232d8c9843811fd6c8b998b0f3b153c
ano201 <ahmu689@gmail.com>
2021-10-14 13:07:03 +0600

    menu added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import menu from '../../images/menu.png';
+
+
 const Header = () => {
 
 
  return (
   <div className='header container'>
-   <nav>
-     <Link to='./home'>Home</Link>
-     <Link to='./opinion'>Opinion</Link>
-     <Link to='./about-us'>About Us</Link>
-     <Link to='./contact-us'>Contact Us</Link>
-   </nav>
+   <div className='nav-container'>
+    <div>
+     <img alt='' src={menu} className='menu'/>
+    </div>
+    <nav>
+      <Link to='./home'>Home</Link>
+      <Link to='./opinion'>Opinion</Link>
+      <Link to='./about-us'>About Us</Link>
+      <Link to='./contact-us'>Contact Us</Link>
+    </nav>
+    <div>
+     <img alt='' src={menu} className='favourite' />
+    </div>
+   </div>
   </div>
  )
 
